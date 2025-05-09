f31189cafd4a38ef68f9c90c2fcd1107a2d30a66
Flávio Henkes Bagestan <flavio@ows.com.br>
2025-05-09 13:17:06 -0300

    :construction: HangmanScene

--- WageMage/Scripts/IntroScreen.gd
+++ WageMage/Scripts/IntroScreen.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+
+func _init() -> void:
+    print("bugbaya")
+
+func _ready() -> void:
+    await get_tree().create_timer(3.0).timeout
+    GameManager.change_scene(OptionsHandler.SceneScreen.HANGMAN_SCREEN)
