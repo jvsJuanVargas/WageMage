f5762452e669fb1a9658e5fa80a5331d324918b1
Juan Vargas <jvsjuanvargas@gmail.com>
2025-05-14 13:54:15 -0300

    :construction: Key scene

--- WageMage/Scenes/Screens/HangmanScreen.gd
+++ WageMage/Scenes/Screens/HangmanScreen.gd
@@ -1,6 +1,36 @@
 extends Node2D
 
+const KEY : PackedScene = preload("res://Scenes/GUI/Key.tscn")
+const LETTERS : Array[String] = [
+	"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
+	"A", "S", "D", "F", "G", "H", "J", "K", "L",
+	"Z", "X", "C", "V", "B", "N", "M",
+]
+
+var keys_line_1 : HBoxContainer
+var keys_line_2 : HBoxContainer
+var keys_line_3 : HBoxContainer = null
+
 
 # Ready ========================================================================
 func _ready() -> void:
-    GameManager.finish_loading(OptionsHandler.GameState.HANGMAN)
+	GameManager.finish_loading(OptionsHandler.GameState.HANGMAN)
+	keys_line_1 = $GameLayer/GameContainer/Keys/Lines/Line_1
+	keys_line_2 = $GameLayer/GameContainer/Keys/Lines/Line_2
+	keys_line_3 = $GameLayer/GameContainer/Keys/Lines/Line_3
+	configure()
+
+
+# Configure ====================================================================
+func configure() -> void:
+	var counter : int = 0
+	for letter in LETTERS:
+		var key : Node = KEY.instantiate()
+		key.configure(letter)
+		if counter < 10:
+			keys_line_1.add_child(key)
+		elif counter < 19:
+			keys_line_2.add_child(key)
+		else:
+			keys_line_3.add_child(key)
+		counter += 1
---
+++ WageMage/Scripts/Key.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+# Configure ====================================================================
+func configure(letter: String) -> void:
+    $Letter.text = letter
