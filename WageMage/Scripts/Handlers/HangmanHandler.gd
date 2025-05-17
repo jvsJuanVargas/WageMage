7c81ee63e38b321b52c5d6c56c0170578feb197e
Juan Vargas <jvsjuanvargas@gmail.com>
2025-05-17 18:48:19 -0300

    :construction: Refactor Hangman game structure, update Key and Letter scenes, and implement HangmanManager

---
+++ WageMage/Scripts/Handlers/HangmanHandler.gd
@@ -0,0 +1,9 @@
+class_name HangmanHandler
+
+
+# Key state --------------------------------------------------------------------
+enum KeyState {
+    AVALIABLE,
+    CORRECT,
+    INCORRECT,
+}
--- WageMage/Scripts/Key.gd
+++ WageMage/Scripts/Key.gd
@@ -1,7 +1,7 @@
 extends Control
 
 
-signal letter_selected(letter:String)
+signal letter_selected(key:String)
 
 
 const AVALIABLE_COLOR : Color = Color(1, 1, 1, 1)
@@ -9,35 +9,48 @@ const CORRECT_COLOR : Color = Color(0, 1, 0, 1)
 const INCORRECT_COLOR : Color = Color(1, 0, 0, 1)
 
 
-var letter: String = ""
-var is_selected: bool = false
-var size_tween: Tween = null
-var color_tween: Tween = null
+var letter : String = ""
+var size_tween : Tween = null
+var color_tween : Tween = null
+var state : HangmanHandler.KeyState = HangmanHandler.KeyState.AVALIABLE
+
+
+# Ready ========================================================================
+func _ready() -> void:
+	pass
 
 
 # Configure ====================================================================
 func configure(new_letter:String) -> void:
 	letter = new_letter
-	is_selected = false
+	state = HangmanHandler.KeyState.AVALIABLE
 	$Container/Background.modulate = AVALIABLE_COLOR
-	$Container/Letter.text = letter
+	$Container/Character.text = letter
+
+
+# Update the key state/color ---------------------------------------------------
+func update_key(is_correct:bool) -> void:
+	state = HangmanHandler.KeyState.CORRECT if is_correct else HangmanHandler.KeyState.INCORRECT
+	if color_tween:
+		color_tween.kill()
+	color_tween = get_tree().create_tween()
+	color_tween.tween_property($Container/Background, 'modulate', CORRECT_COLOR if is_correct else INCORRECT_COLOR, 0.1)
 
 
 # Signals ======================================================================
 
 # When the mouse enters the key area -------------------------------------------
 func _on_container_mouse_entered() -> void:
-	print("Mouse entered key %s area" % [letter])
-	if size_tween:
-		size_tween.kill()
-	size_tween = get_tree().create_tween()
-	size_tween.tween_property($Container/Background, 'size',Vector2(86, 86), 0.05)
-	size_tween.parallel().tween_property($Container/Background, 'position',Vector2(0, 0), 0.05)
+	if state == HangmanHandler.KeyState.AVALIABLE:
+		if size_tween:
+			size_tween.kill()
+		size_tween = get_tree().create_tween()
+		size_tween.tween_property($Container/Background, 'size',Vector2(86, 86), 0.05)
+		size_tween.parallel().tween_property($Container/Background, 'position',Vector2(0, 0), 0.05)
 
 
 # When the mouse exits the key area --------------------------------------------
 func _on_container_mouse_exited() -> void:
-	print("Mouse exited key %s area" % [letter])
 	if size_tween:
 		size_tween.kill()
 	size_tween = get_tree().create_tween()
@@ -48,11 +61,7 @@ func _on_container_mouse_exited() -> void:
 # When the key is pressed ------------------------------------------------------
 func _on_container_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if Input.is_action_just_released('LeftClick') and not is_selected:
-			if color_tween:
-				color_tween.kill()
-			is_selected = true
-			letter_selected.emit(letter)
-			print("Key %s pressed" % [letter])
-			color_tween = get_tree().create_tween()
-			color_tween.tween_property($Container/Background, 'modulate', CORRECT_COLOR, 0.1)
+		if Input.is_action_just_released('LeftClick') and state == HangmanHandler.KeyState.AVALIABLE:
+			state = HangmanHandler.KeyState.INCORRECT
+			letter_selected.emit(self)
+	
