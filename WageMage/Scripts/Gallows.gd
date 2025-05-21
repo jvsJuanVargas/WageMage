e7d3f599bf8eabec44144d73a373421ad1c99de3
Juan Vargas <jvsjuanvargas@gmail.com>
2025-05-21 14:10:19 -0300

    :construction: Enhance Gallows scene with animations and update game logic for smoother transitions

--- WageMage/Scripts/Gallows.gd
+++ WageMage/Scripts/Gallows.gd
@@ -5,6 +5,7 @@ signal billionaire_died()
 
 const BODY_PART : PackedScene = preload('res://Scenes/GUI/BodyPart.tscn')
 
+var animation_speed_tween : Tween = null
 var attempts : int = 6
 var body : Control = null
 var parts : Array[Control] = []
@@ -26,11 +27,19 @@ func configure(_billionaire_data:BillionaireData) -> void:
 	parts.append($Body/RightLeg)
 	for part in parts:
 		part.configure()
+	$BodyAnimation.speed_scale = 0.6
 
 
 # wrong gess -------------------------------------------------------------------
 func wrong_guess() -> void:
 	parts[attempts].show_part()
 	attempts += 1
+	if attempts == 1:
+		$BodyAnimation.play('Swing')
+	if attempts == 2:
+		if animation_speed_tween:
+			animation_speed_tween.kill()
+		animation_speed_tween = get_tree().create_tween()
+		animation_speed_tween.tween_property($BodyAnimation, 'speed_scale', 1.0, 0.5)
 	if attempts >= parts.size():
 		billionaire_died.emit()
